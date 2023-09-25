df7c3cd1003fba4a68f5b74c2a9e5c711c5fe6e6
Peter <piotr@piotr.com>
2023-09-25 23:57:48 +0200

    Finalie sorting by number and date

--- src/scripts/header-functions.js
+++ src/scripts/header-functions.js
@@ -2,6 +2,27 @@ import { CONSTANTS } from "./constants.js";
 
 function updateHeaderSort(e) {
   const elem = e.target;
+
+  if (!elem) {
+    return;
+  }
+
+  const isSortable = elem.classList.contains(CONSTANTS.CLASS_NAMES.SORTABLE);
+
+  if (!isSortable) {
+    return;
+  }
+
+  const sortStatus = updateSortStatus(elem);
+
+  if (sortStatus === CONSTANTS.SORT_STATUS.NONE) {
+    return;
+  }
+
+  sortData({ byField: elem.textContent.toUpperCase(), dir: sortStatus });
+}
+
+function updateSortStatus(elem) {
   const isClickable = elem.classList.contains(CONSTANTS.CLASS_NAMES.CLICKABLE);
 
   if (!isClickable) {
@@ -28,26 +49,28 @@ function updateHeaderSort(e) {
 
     updateSortIconOnParent({ elem, shouldAdd: true, dir: "asc" });
 
-    return;
+    return CONSTANTS.SORT_STATUS.ASC;
   }
 
   const isSortedDesc = elem.classList.contains(
     CONSTANTS.CLASS_NAMES.SORTED_DESC
   );
 
-  console.log({ isSorted, isSortedDesc });
-
   if (isSortedDesc) {
     elem.classList.remove(CONSTANTS.CLASS_NAMES.SORTED);
     elem.classList.remove(CONSTANTS.CLASS_NAMES.SORTED_DESC);
 
     updateSortIconOnParent({ elem, shouldAdd: false });
+
+    return CONSTANTS.SORT_STATUS.NONE;
   } else {
     elem.classList.remove(CONSTANTS.CLASS_NAMES.SORTED_ASC);
     elem.classList.add(CONSTANTS.CLASS_NAMES.SORTED_DESC);
 
     updateSortIconOnParent({ elem, shouldAdd: false });
     updateSortIconOnParent({ elem, shouldAdd: true, dir: "desc" });
+
+    return CONSTANTS.SORT_STATUS.DESC;
   }
 }
 
@@ -69,6 +92,78 @@ function updateSortIconOnParent({ elem, shouldAdd = false, dir = "asc" }) {
   icon.classList.add(dir === "asc" ? "bi-sort-up-alt" : "bi-sort-down");
   icon.classList.add(CONSTANTS.CLASS_NAMES.SORT_ICON);
   parent.append(icon);
+}
+
+function sortData({ byField, dir }) {
+  const entryList = document.getElementsByClassName(
+    CONSTANTS.CLASS_NAMES.ABSENCE_ENTRY
+  );
+
+  if (
+    entryList.length < 2 ||
+    !dir ||
+    ![CONSTANTS.SORT_STATUS.ASC, CONSTANTS.SORT_STATUS.DESC].includes(dir)
+  ) {
+    return;
+  }
+
+  const elemArr = [...entryList];
+
+  let sortByElem;
+
+  switch (byField) {
+    case CONSTANTS.SORT_FIELDS.FROM:
+      sortByElem = CONSTANTS.CLASS_NAMES.ABSENCE_DATE_FROM;
+      break;
+    case CONSTANTS.SORT_FIELDS.TO:
+      sortByElem = CONSTANTS.CLASS_NAMES.ABSENCE_DATE_TO;
+      break;
+    case CONSTANTS.SORT_FIELDS.PERCENT:
+      sortByElem = CONSTANTS.CLASS_NAMES.ABSENCE_PERCENT;
+      break;
+    case CONSTANTS.SORT_FIELDS.REASON:
+      sortByElem = CONSTANTS.CLASS_NAMES.REASON;
+      break;
+    default:
+      break;
+  }
+
+  if (!sortByElem) {
+    return;
+  }
+
+  elemArr.sort((elemA, elemB) => {
+    const sortFieldA = elemA.querySelector(`.${sortByElem}`);
+    const sortFieldB = elemB.querySelector(`.${sortByElem}`);
+
+    const valA = sortFieldA.value;
+    const valB = sortFieldB.value;
+
+    let parsedValA;
+    let parsedValB;
+
+    switch (sortFieldA.type) {
+      case "number":
+        parsedValA = parseInt(valA);
+        parsedValB = parseInt(valB);
+        break;
+      case "date":
+        parsedValA = new Date(valA);
+        parsedValB = new Date(valB);
+        break;
+      default:
+        parsedValA = valA;
+        parsedValB = valB;
+        break;
+    }
+
+    return dir === CONSTANTS.SORT_STATUS.ASC
+      ? parsedValA - parsedValB
+      : parsedValB - parsedValA;
+  });
+
+  const parentElem = document.getElementById(CONSTANTS.IDS.ABSENCE_ENTRY_LIST);
+  elemArr.forEach((e) => parentElem.appendChild(e));
 }
 
 export { updateHeaderSort };
